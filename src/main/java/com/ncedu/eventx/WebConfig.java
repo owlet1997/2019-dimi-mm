82624036748359c26c59af51bd195cf90934e94e
Nadezhda Shatalova <alonewithlight@yandex.ru>
2020-02-18 01:12:20 +0400

    fix: добавлен класс конфигурации для маппинга путей для ресурсов

---
+++ src/main/java/com/ncedu/eventx/WebConfig.java
@@ -0,0 +1,37 @@
+package com.ncedu.eventx;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler(
+                    "/img/**",
+                    "/bootstrap/**",
+                    "/external/**",
+                    "/fonts/**",
+                    "/icons/**",
+                    "/img/**",
+                    "/js/**",
+                    "/css/**",
+                    "/scss/**")
+                    .addResourceLocations(
+                            "classpath:/static/img/",
+                            "classpath:/static/bootstrap/",
+                            "classpath:/static/external/",
+                            "classpath:/static/fonts/",
+                            "classpath:/static/icons/",
+                            "classpath:/static/img/",
+                            "classpath:/static/js/",
+                            "classpath:/static/css/",
+                            "classpath:/static/scss/");
+        }
+
+
+}
