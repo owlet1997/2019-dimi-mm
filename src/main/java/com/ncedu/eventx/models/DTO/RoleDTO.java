afcd811d6262276d35b6201b1056a969ab60ecea
AlexandrTairov <sanek.tai0@gmail.com>
2020-03-23 19:20:58 +0400

    Исправлены UserDTO и RoleDTO

--- src/main/java/com/ncedu/eventx/models/DTO/RoleDTO.java
+++ src/main/java/com/ncedu/eventx/models/DTO/RoleDTO.java
@@ -12,21 +12,4 @@ public class RoleDTO {
     private int id;
     private String name;
 
-    // Удалить
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- src/main/java/com/ncedu/eventx/models/DTO/UserDTO.java
+++ src/main/java/com/ncedu/eventx/models/DTO/UserDTO.java
@@ -1,5 +1,6 @@
 package com.ncedu.eventx.models.DTO;
 
+import com.ncedu.eventx.models.entities.RoleEntity;
 import lombok.*;
 
 import javax.persistence.Transient;
@@ -14,9 +15,7 @@ public class UserDTO {
 
     private int id;
 
-
-    private List<RoleDTO> roles;
-
+    private RoleDTO role;
 
     private String email;
 
@@ -33,77 +32,4 @@ public class UserDTO {
 
     private String avatarImg;
 
-    // Удалить
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public List<RoleDTO> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<RoleDTO> roles) {
-        this.roles = roles;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPasswordConfirm() {
-        return passwordConfirm;
-    }
-
-    public void setPasswordConfirm(String passwordConfirm) {
-        this.passwordConfirm = passwordConfirm;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getOrganizationName() {
-        return organizationName;
-    }
-
-    public void setOrganizationName(String organizationName) {
-        this.organizationName = organizationName;
-    }
-
-    public String getPositionName() {
-        return positionName;
-    }
-
-    public void setPositionName(String positionName) {
-        this.positionName = positionName;
-    }
-
-    public String getAvatarImg() {
-        return avatarImg;
-    }
-
-    public void setAvatarImg(String avatarImg) {
-        this.avatarImg = avatarImg;
-    }
 }
